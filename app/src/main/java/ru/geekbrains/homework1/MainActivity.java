cf69f0380a635244a1284f43227888726a5bc24a
Vladimir <Zionit27@gmail.com>
2021-05-26 12:53:56 +0700

    add method switchLayout.

--- app/src/main/java/ru/geekbrains/homework1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/homework1/MainActivity.java
@@ -3,12 +3,17 @@ package ru.geekbrains.homework1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_personal_information);
+    }
+
+    public void switchLayout(View view) {
+        setContentView(R.layout.activity_main);
     }
 }
